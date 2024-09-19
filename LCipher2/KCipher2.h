42f2054a6421d32d355c420366fb95c4bb29d52f
luci4 <142809264+l00sy4@users.noreply.github.com>
2024-09-19 10:37:26 +0000

    Update KCipher2.h

--- LCipher2/KCipher2.h
+++ LCipher2/KCipher2.h
@@ -1,5 +1,21 @@
 #pragma once
 
-#include "Common.h"
+#define INIT   0
+#define NORMAL 1
 
-void kcipher2_encrypt(unsigned char* in, unsigned long len, unsigned char* out);
+typedef struct
+{
+	unsigned int  A[5];
+	unsigned int  B[11];
+	unsigned int  L1, R1, L2, R2;
+
+} kcipher2_state;
+
+extern unsigned int   IK[12];
+extern unsigned int   IV[4];
+extern kcipher2_state State;
+
+void               kcipher2_encrypt(unsigned char* in, unsigned long len, unsigned char* out);
+void               init(unsigned int* key, unsigned int* iv);
+void               next(unsigned char mode);
+unsigned long long stream();
