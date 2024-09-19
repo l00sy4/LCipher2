14fda3f704a506ad1b1f33718917102406523847
luci4 <142809264+l00sy4@users.noreply.github.com>
2024-09-19 10:13:56 +0000

    Update and rename KCipher2.h to LCipher2/KCipher2.h

--- KCipher2.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "Common.h"
-
-void kcipher2_encrypt_decrypt(unsigned char* in, unsigned long len, unsigned char* out);
---
+++ LCipher2/KCipher2.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "Common.h"
+
+void kcipher2_encrypt(unsigned char* in, unsigned long len, unsigned char* out);
