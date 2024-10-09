e7bba4e1bae9c24cff52c220e6cd2d39db64d27e
luci4 <142809264+l00sy4@users.noreply.github.com>
2024-10-09 07:22:47 +0000

    Update KCipher2.c

--- LCipher2/KCipher2.c
+++ LCipher2/KCipher2.c
@@ -1,5 +1,9 @@
 #include "Tables.h"
-#include "KCipher2.h"
+#include "Common.h"
+
+unsigned int   IK[12];
+unsigned int   IV[4];
+kcipher2_state State;
 
 inline unsigned int nlf(const unsigned int a, const unsigned int b, const unsigned int c, const unsigned int d)
 {
@@ -50,130 +54,128 @@ unsigned int sub_k2(const unsigned int in)
 	return q3 << 24 | q2 << 16 | q1 << 8 | q0;
 }
 
-inline unsigned int* key_expansion(unsigned int* key, const unsigned int* iv)
+inline void key_expansion(const unsigned int* key, const unsigned int* iv)
 {
-	key[4]  = key[0] ^ sub_k2(key[3] << 8 ^ key[3] >> 24) ^ 0x01000000;
-	key[5]  = key[1] ^ key[4]; key[6] = key[2] ^ key[5];
-	key[7]  = key[3] ^ key[6];
-	key[8]  = key[4] ^ sub_k2(key[7] << 8 ^ key[7] >> 24) ^ 0x02000000;
-	key[9]  = key[5] ^ key[8]; key[10] = key[6] ^ key[9];
-	key[11] = key[7] ^ key[10];
+	IV[0] = iv[0];
+	IV[1] = iv[1];
+	IV[2] = iv[2];
+	IV[3] = iv[3];
 
-	return key;
+	IK[0] = key[0];
+	IK[1] = key[1];
+	IK[2] = key[2];
+	IK[3] = key[3];
+
+	IK[4] = IK[0] ^ sub_k2(IK[3] << 8 ^ IK[3] >> 24) ^ 0x01000000;
+	IK[5] = IK[1] ^ IK[4]; IK[6] = IK[2] ^ IK[5];
+	IK[7] = IK[3] ^ IK[6];
+	IK[8] = IK[4] ^ sub_k2(IK[7] << 8 ^ IK[7] >> 24) ^ 0x02000000;
+	IK[9] = IK[5] ^ IK[8]; IK[10] = IK[6] ^ IK[9];
+	IK[11] = IK[7] ^ IK[10];
 }
 
-kcipher2_state setup_state_values(kcipher2_state state, const unsigned int* key, const unsigned int* iv)
+void setup_state_values(const unsigned int* key, const unsigned int* iv)
 {
-	key = key_expansion(key, iv);
+	key_expansion(key, iv);
 
-	state.A[0] = key[4];  state.A[1] = key[3];  state.A[2] = key[2];
-	state.A[3] = key[1];  state.A[4] = key[0];
-	state.B[0] = key[10]; state.B[1] = key[11]; state.B[2] = iv[0]; state.B[3] = iv[1];
-	state.B[4] = key[8];  state.B[5] = key[9];  state.B[6] = iv[2]; state.B[7] = iv[3];
-	state.B[8] = key[7];  state.B[9] = key[5];  state.B[10] = key[6];
+	State.A[0] = IK[4];  State.A[1] = IK[3];  State.A[2] = IK[2];
+	State.A[3] = IK[1];  State.A[4] = IK[0];
+	State.B[0] = IK[10]; State.B[1] = IK[11]; State.B[2] = IV[0]; State.B[3] = IV[1];
+	State.B[4] = IK[8];  State.B[5] = IK[9];  State.B[6] = IV[2]; State.B[7] = IV[3];
+	State.B[8] = IK[7];  State.B[9] = IK[5];  State.B[10] = IK[6];
 
-	state.L1 = state.R1 = state.L2 = state.R2 = 0x00000000;
-
-	return state;
+	State.L1 = State.R1 = State.L2 = State.R2 = 0x00000000;
 }
 
-kcipher2_state init(const unsigned int *key, const unsigned int *iv, kcipher2_state state)
+void init(unsigned int *key, unsigned int *iv)
 {
-	state = setup_state_values(state, key, iv);
+	setup_state_values(key, iv);
 
 	for (unsigned char i = 0; i < 24; i++) 
 	{
-		state = next(INIT, state);
+		next(INIT);
 	}
-
-	return state;
 }
 
-kcipher2_state next(const unsigned char mode, kcipher2_state state)
+void next(const unsigned char mode)
 {
-	unsigned int nL1 = sub_k2(state.R2 + state.B[4]);
-	unsigned int nR1 = sub_k2(state.L2 + state.B[9]);
-	unsigned int nL2 = sub_k2(state.L1);
-	unsigned int nR2 = sub_k2(state.R1);
+	unsigned int temp2;
+
+	unsigned int nL1 = sub_k2(State.R2 + State.B[4]);
+	unsigned int nR1 = sub_k2(State.L2 + State.B[9]);
+	unsigned int nL2 = sub_k2(State.L1);
+	unsigned int nR2 = sub_k2(State.R1);
 
 	unsigned int nA[5];
 
-	nA[0] = state.A[1]; nA[1] = state.A[2]; nA[2] = state.A[3]; nA[3] = state.A[4];
+	nA[0] = State.A[1]; nA[1] = State.A[2]; nA[2] = State.A[3]; nA[3] = State.A[4];
 
 	unsigned int nB[11];
 
-	nB[0] = state.B[1]; nB[1] = state.B[2]; nB[2] = state.B[3]; nB[3] = state.B[4];
-	nB[4] = state.B[5]; nB[5] = state.B[6]; nB[6] = state.B[7]; nB[7] = state.B[8];
-	nB[8] = state.B[9]; nB[9] = state.B[10];
+	nB[0] = State.B[1]; nB[1] = State.B[2]; nB[2] = State.B[3]; nB[3] = State.B[4];
+	nB[4] = State.B[5]; nB[5] = State.B[6]; nB[6] = State.B[7]; nB[7] = State.B[8];
+	nB[8] = State.B[9]; nB[9] = State.B[10];
 
-	unsigned int temp1 = state.A[0] << 8 ^ amul0[(state.A[0] >> 24)];
-	nA[4] = temp1 ^ state.A[3];
+	unsigned int temp1 = State.A[0] << 8 ^ amul0[(State.A[0] >> 24)];
+	nA[4] = temp1 ^ State.A[3];
 
 	if (mode == INIT)
 	{
-		nA[4] ^= nlf(state.B[0], state.R2, state.R1, state.A[4]);
+		nA[4] ^= nlf(State.B[0], State.R2, State.R1, State.A[4]);
 	}
 
-	if (state.A[2] & 0x40000000)
+	if (State.A[2] & 0x40000000)  
 	{
-		temp1 = state.B[0] << 8 ^ amul1[(state.B[0] >> 24)];
+		temp1 = State.B[0] << 8 ^ amul1[(State.B[0] >> 24)];
 	}
 	else 
 	{
-		temp1 = state.B[0] << 8 ^ amul2[(state.B[0] >> 24)];
+		temp1 = State.B[0] << 8 ^ amul2[(State.B[0] >> 24)];
 	}
 
-	unsigned int temp2;
-
-	if (state.A[2] & 0x80000000)
+	if (State.A[2] & 0x80000000)
 	{
-		temp2 = state.B[8] << 8 ^ amul3[(state.B[8] >> 24)];
+		temp2 = State.B[8] << 8 ^ amul3[(State.B[8] >> 24)];
 	}
 	else  
 	{
-		temp2 = state.B[8];
+		temp2 = State.B[8];
 	}
 
-	nB[10] = temp1 ^ state.B[1] ^ state.B[6] ^ temp2;
+	nB[10] = temp1 ^ State.B[1] ^ State.B[6] ^ temp2;
 
 	if (mode == INIT)
 	{
-		nB[10] ^= nlf(state.B[10], state.L2, state.L1, state.A[0]);
+		nB[10] ^= nlf(State.B[10], State.L2, State.L1, State.A[0]);
 	}
 
-	kcipher2_state new_state;
+	State.A[0] = nA[0]; State.A[1] = nA[1]; State.A[2]  = nA[2];
+	State.A[3] = nA[3]; State.A[4] = nA[4];
+	State.B[0] = nB[0]; State.B[1] = nB[1]; State.B[2]  = nB[2]; State.B[3] = nB[3];
+	State.B[4] = nB[4]; State.B[5] = nB[5]; State.B[6]  = nB[6]; State.B[7] = nB[7];
+	State.B[8] = nB[8]; State.B[9] = nB[9]; State.B[10] = nB[10];
 
-	new_state.A[0] = nA[0]; new_state.A[1] = nA[1]; new_state.A[2]  = nA[2];
-	new_state.A[3] = nA[3]; new_state.A[4] = nA[4];
-	new_state.B[0] = nB[0]; new_state.B[1] = nB[1]; new_state.B[2]  = nB[2]; new_state.B[3] = nB[3];
-	new_state.B[4] = nB[4]; new_state.B[5] = nB[5]; new_state.B[6]  = nB[6]; new_state.B[7] = nB[7];
-	new_state.B[8] = nB[8]; new_state.B[9] = nB[9]; new_state.B[10] = nB[10];
-
-	new_state.L1 = nL1; new_state.R1 = nR1; new_state.L2 = nL2; new_state.R2 = nR2;
-
-	return new_state;
+	State.L1 = nL1; State.R1 = nR1; State.L2 = nL2; State.R2 = nR2;
 }
 
-unsigned long long stream(const kcipher2_state state)
+unsigned long long stream()
 {
-	const unsigned int zh = nlf(state.B[10], state.L2, state.L1, state.A[0]);
-	const unsigned int zl = nlf(state.B[0],  state.R2, state.R1, state.A[4]);
+	const unsigned int zh = nlf(State.B[10], State.L2, State.L1, State.A[0]);
+	const unsigned int zl = nlf(State.B[0],  State.R2, State.R1, State.A[4]);
 
 	return (unsigned long long)zh << 32 | zl;
 }
 
-void kcipher2_encrypt(kcipher2_state state, unsigned char* in, const unsigned long len, unsigned char* out, const unsigned int* Key, const unsigned int* Iv)
+void kcipher2_encrypt(unsigned char* in, const unsigned long len, unsigned char* out)
 {
-
-	state = init(Key, Iv, state);
 	unsigned long long key_stream;
 	unsigned long long* buffer     = (unsigned long long*)in;
 	unsigned long long* out_buffer = (unsigned long long*)out;
 
 	for (unsigned long i = 0; i < len / 8; i++)
 	{
-		key_stream = stream(state);
-		state      = next(NORMAL, state);
+		key_stream = stream();
+		next(NORMAL);
 
 		out_buffer[i] = buffer[i] ^ key_stream;
 	}
@@ -185,8 +187,8 @@ void kcipher2_encrypt(kcipher2_state state, unsigned char* in, const unsigned lo
 		unsigned char* byte_buffer = (unsigned char*)&buffer[len / 8];
 		unsigned char* out_byte_buffer = (unsigned char*)&out_buffer[len / 8];
 
-		key_stream = stream(state);
-		state      = next(NORMAL, state);
+		key_stream = stream();
+		next(NORMAL);
 
 		for (unsigned long i = 0; i < remaining_bytes; i++)
 		{
